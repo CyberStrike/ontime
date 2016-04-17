2470918b2db84135a419d34f377cdf95aaf1f0a4
Chris Scott <cyberstrike@gmail.com>
2016-04-17 01:12:57 -0400

    gst

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,12 +11,13 @@ class ApplicationController < ActionController::Base
     render nothing: true
   end
 
-   def current_user
+  def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-
+  end
 
   use Rack::Cors do
     origins '*'
     resource '*', headers: :any, methods: :any
   end
+  
 end
